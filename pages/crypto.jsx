fb16a6f27329c8a8ac77fecc46c7635d896275e8
Philipp Gampe <phil@MacBook-Air.local>
2022-10-10 14:34:33 +0200

    Crypto-Page

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -1,14 +1,80 @@
+import Image from "next/image";
 import React from "react";
-import Head from "next/head";
-import Navbar from "../components/Navbar";
+import philflixImg from "../public/pictures/Philflix.jpeg";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
 
 const crypto = () => {
   return (
-    <div>
-      <Head>
-        <title>Phil | Front-End Developer</title>
-      </Head>
-      <Navbar />
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={philflixImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Crypto-App</h2>
+          <h3>ReactJS / Tailwind / Firebase</h3>
+        </div>
+      </div>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p className="pb-3">Projekt</p>
+          <h2>Übersicht</h2>
+          <p className="py-4">
+            facilisis magna etiam tempor orci eu lobortis elementum nibh tellus
+            molestie nunc non blandit massa enim nec dui nunc mattis enim ut
+            tellus elementum sagittis vitae et leo duis ut diam quam nulla
+            porttitor massa id neque aliquam vestibulum morbi blandit cursus
+            risus at ultrices mi tempus imperdiet nulla malesuada pellentesque
+            elit eget gravida cum sociis natoque penatibus et magnis dis
+            parturient montes nascetur ridiculus mus mauris vitae ultricies leo
+            integer malesuada nunc vel risus
+          </p>
+          <a href="/" target="_blank" rel="noreferrer">
+            <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          </a>
+          <a
+            href="https://github.com/TastyCrysis/crypto-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Code</button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> JavaScript
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Firebase
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Coin Gecko Api
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Routes
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
     </div>
   );
 };
